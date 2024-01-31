d5ca7e7d482abfd5ef2fd148d98582de5892856c
n37av <87298488+n37av@users.noreply.github.com>
2024-01-31 07:40:28 +0100

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola Mundo';
+
 }
